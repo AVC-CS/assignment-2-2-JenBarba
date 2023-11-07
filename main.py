afbc1c5ba9cf7e24c2dac02ae28ebff18e58e7b8
JenBarba <149451127+JenBarba@users.noreply.github.com>
2023-11-06 20:14:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
-    celcius = int(input())
-    farenheit = (celcius * 9/5) + 32
+    celsius = int(input())
+    fahrenheit = (celsius * 9/5) + 32
     print(f'{farenheit:.2f}')
 
     """
@@ -14,7 +14,7 @@ def main():
     # Do not delete the return statement
     ########################################
     """
-    return celcius, fahrenheit
+    return celsius, fahrenheit
 
 
 if __name__ == '__main__':
